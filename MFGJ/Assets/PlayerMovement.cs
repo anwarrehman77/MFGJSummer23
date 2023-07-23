bae6ac41fb3c76cfb0ea8faa155c62c7af585daa
Anwar <anwarrehman77@gmail.com>
2023-07-23 14:46:32 -0700

    Added some player movement and cinemachine camera

---
+++ MFGJ/Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speedY = 2f, speedX = 5f, speedBiasX = 10f;
+
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.velocity = new Vector2(speedX * Input.GetAxisRaw("Horizontal") + speedBiasX, speedY * Input.GetAxisRaw("Vertical"));
+    }
+}
